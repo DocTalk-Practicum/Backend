1f537c237dd211644c30174a92e21035b1576a52
Prakhar <srivastava11735@gmail.com>
2022-05-01 22:15:51 +0530

    Intial Commit

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
 
-router.post("/patientregister", authController.patientRegister);
+router.post("/patientRegister", authController.patientRegister);
 router.post("/doctorRegister", authController.doctorRegister);
 router.post("/login", authController.login);
 
