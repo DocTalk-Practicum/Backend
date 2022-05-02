b66765245bb13b06bdb1402b92470d074a9d5113
Paras <parasthind299@gmail.com>
2022-05-02 21:28:22 +0530

    changes in login

--- routes/patient.js
+++ routes/patient.js
@@ -4,3 +4,5 @@ const router = express.Router();
 const upload = require("../middlewares/multer");
 
 router.post("/uploadReport", upload.array("files", 1), reportUpload);
+
+module.exports = router;
